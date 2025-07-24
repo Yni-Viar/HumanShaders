df16290895a1ae74dfacd00f4ae6dbd14f751cf3
matmadness <68684211+matmadness@users.noreply.github.com>
2025-07-24 18:56:06 -0300

    Added micro normals to the diffuse normal
    - Added micro normals to the diffuse normal calculation;
    
    - Added oprion to rotate the HDRI sky;
    
    - Added new LUTs;
    
    -Tweaked some skin values.

--- Scripts/auto_rotate.gd
+++ Scripts/auto_rotate.gd
@@ -1,9 +1,9 @@
-@tool
 extends Node3D
 
-# Sensitivity for rotation
 @export var use_right_click : bool = false
 @export var rotation_sensitivity: float = 0.05
+@export var world_env: WorldEnvironment
+@export var rotate_sky: bool = false
 
 @onready var ui: Control = %UI
 
@@ -21,30 +21,26 @@ var previous_mouse_position: Vector2
 #				i.gui_input.connect(using_ui)
 
 func _input(event: InputEvent) -> void:
-	if not Engine.is_editor_hint():
-		var mouse_click = MOUSE_BUTTON_LEFT
-		if use_right_click:
-			mouse_click = MOUSE_BUTTON_RIGHT
-		
-		if event is InputEventMouseMotion and dragging:
-			# Calculate the drag distance
-			var delta_x = event.relative.x
-			# Rotate the model along the Y-axis
-			rotate_node(-delta_x * rotation_sensitivity)
-		
-		elif event is InputEventMouseButton and mouse_available:
-			if event.button_index == mouse_click:
-				if event.pressed and not is_mouse_over_ui():
-					dragging = true
-					previous_mouse_position = event.position
-				else:
-					dragging = false
-	#else:
-		#Autoload.log_message("Using UI")
-
-func rotate_node(angle: float):
-	# Rotate around the Y-axis
-	rotation_degrees.y -= angle
+	var mouse_click = MOUSE_BUTTON_LEFT
+	if use_right_click:
+		mouse_click = MOUSE_BUTTON_RIGHT
+	
+	if event is InputEventMouseMotion and dragging:
+		# Calculate the drag distance
+		var delta_x = event.relative.x
+		# Rotate the model along the Y-axis
+		rotation_degrees.y -= -delta_x * rotation_sensitivity
+		# If true, rotate sky:
+		if rotate_sky:
+			world_env.environment.sky_rotation.y -= deg_to_rad(-delta_x * rotation_sensitivity)
+	
+	elif event is InputEventMouseButton and mouse_available:
+		if event.button_index == mouse_click:
+			if event.pressed and not is_mouse_over_ui():
+				dragging = true
+				previous_mouse_position = event.position
+			else:
+				dragging = false
 	
 func is_mouse_over_ui() -> bool:
 	# Check if the mouse is interacting with the UI
